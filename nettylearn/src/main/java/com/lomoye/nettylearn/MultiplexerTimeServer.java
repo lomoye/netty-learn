8219fe9dbf7628020b3552da4e9682661b019d06
lomoye <834033206@qq.com>
2017-08-25 21:28:30 +0800

    fix

---
+++ nettylearn/src/main/java/com/lomoye/nettylearn/MultiplexerTimeServer.java
@@ -0,0 +1,125 @@
+package com.lomoye.nettylearn;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by lomoye on 2017/8/25.
+ *
+ */
+public class MultiplexerTimeServer implements Runnable {
+
+    private Selector selector;
+
+    private ServerSocketChannel serverSocketChannel;
+
+    private volatile boolean stop;
+
+    public MultiplexerTimeServer(int port) {
+        try {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            System.out.println("服务器开启,端口:" + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(1000L);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            //处理新接入的消息
+            if (key.isAcceptable()) {
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                sc.register(selector, SelectionKey.OP_READ);
+            }
+
+            if (key.isReadable()) {
+                SocketChannel sc = (SocketChannel) key.channel();
+                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(byteBuffer);
+                if (readBytes > 0) {
+                    byteBuffer.flip();
+                    byte[] bytes = new byte[byteBuffer.remaining()];
+                    byteBuffer.get(bytes);
+
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("服务器收到数据:" + body);
+
+                    String currentTime;
+
+                    if (!"QT".equals(body)) {
+                        currentTime = "BAD ORDER";
+                    } else {
+                        currentTime = new Date().toString();
+                    }
+
+                    doWrite(sc, currentTime);
+                }
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel sc, String resp) throws IOException {
+        if (resp != null && resp.trim().length() > 0) {
+            byte[] bytes = resp.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            sc.write(writeBuffer);
+        }
+    }
+}
